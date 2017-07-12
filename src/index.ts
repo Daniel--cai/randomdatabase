3a4da901d5bbb4881b9f28214f13dce20419c61a
Daniel <daniel.cai92@gmail.com>
2017-07-12 23:07:49 +1000

    forked scribbletune

--- src/index.ts
+++ src/index.ts
@@ -23,64 +23,50 @@ var root = {
 
 // Run the GraphQL query '{ hello }' and print out the response
 graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
+  console.log(response);;
 });
 
-import * as scribble from 'scribbletune';
-
-let intro1 = scribble.clip({
+import { clip, midi } from '../scribbletune';
+const i1 = {
   notes: ['d4','f#4','b4','d5','c#5','b4','a#4','b4'],
-  pattern: 'xxxxxxxx_____'
-})
-
-let intro2 = scribble.clip({
+  pattern: 'xxxxxxxx_____',
+},
+i2 = {
   notes: ['b4','c#5','d5','e5','f#5','d5','f#5','d5','f#5'],
-  pattern: 'x__x__x__x__x__x__x__x__x_____'
-})
-
-let c1 = scribble.clip({
+  pattern: 'x___x___x___x___x___x___x___x___x___',
+},
+v1 = {
   notes: ['f#4', 'd5', 'f#4','c#5','f#4','b4','b2'],
   pattern: 'x_x____x_x____x_x_x_',
-});
-
-let c2 = scribble.clip({
+  bass: []
+},
+v2 = {
   notes: ['Bmin3','Bmin3','Bmin3','d5', 'c#5','b4','a4','g4','g2'],
-  //pattern: 'x_____x_____x_x_x_x_x_x_x_'
-  pattern: 'x_____x_____x_xxxxxx_'
-})
-let c3 = scribble.clip({
+  pattern: 'x_____x_____x_____x_x_x_x_x_x_',
+},
+v3 = {
   notes: ['Gmaj3','Gmaj3','Gmaj3','d5', 'c#5','d5','c#5','d5','d3'],
-  pattern: 'x_____x_____x_xxxxxx_'
-})
-let c4 = scribble.clip({
+  pattern: 'x_____x_____x_____x_x_x_x_x_x_',
+},
+v4 = {
   notes: ['Dmaj3','Dmaj3','Dmaj3', 'd5','c#5','d5','e5','c#5','a2'],
-  pattern: 'x_____x_____x_xxxxxx_'
-})
-let c5 = scribble.clip({
+  pattern: 'x_____x_____x_____x_x_x_x_x_x_'
+},
+v5 = {
   notes: ['Amaj3','Amaj3'],
-  pattern: 'x____x____'
-})
-
-let c6 = scribble.clip({
-  notes: ['f#5','e5','f#5','e5','f#5',
-          'e5','f#5','e5','f#5','e5',
-          'f#5','g5','g5','d5'],
-  //pattern: 'x_x_x_x_x__x_x__x_x_x_x_x_x__x_____'
+  pattern: 'x_____x_____'
+},
+c1 = {
+  notes: ['f#5','e5','f#5','e5','f#5','e5','f#5','e5','f#5','e5','f#5','g5','g5','d5'],
   pattern: 'xxxxx_xx_xxxxxx_x___'
-})
-
-let c7 = scribble.clip({
-  notes: ['g5','g5','g5','g5','g5','a5','g5','f#5',
-          'f#5','f#5','f#5','f#5','a5','g5','f#5','e5' ],
+},
+c2 = {
+  notes: ['g5','g5','g5','g5','g5','a5','g5','f#5','f#5','f#5','f#5','f#5','a5','g5','f#5','e5' ],
   pattern: 'xxxx_xxx_x___xxxx_xxx_x____'
-})
-
-let c8 = scribble.clip({
+},
+c3 = {
   notes: ['e5','e5','e5','e5','e5','e5','e5','e5','c#5','d5' ],
   pattern: 'xxxxxxxxx_x_'
-})
-
-scribble.midi(intro1.concat(intro2,c1,c2,c3,c4,c5,c1,c6,c7,c8,c6,c7))
-
-
+}
+midi([i1,i2,v1,v2,v3,v4,v5,v1,c1,c2,c3,c1,c2].reduce((a,b)=>a.concat(clip(b)),[]), 'music.mid',50);
 
